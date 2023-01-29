3f939a447851e87ec711e74ba7d74d73e052c30e
Prince Olubari <olubari.prince.g@gmail.com>
2023-01-29 22:52:49 +0000

    nested the promise of one fetch into another

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,18 @@
-
-var locationQuerryURL="http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=49c5abbee178f817547829ce7c14a9e3"
+var locationQuerryURL =
+  "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=49c5abbee178f817547829ce7c14a9e3";
 fetch(locationQuerryURL)
   .then((response) => response.json())
-  .then((data) => {
+  .then((cities) => {
     // do something with the data
-    console.log(data);
-  });
+    let firstCity = cities[0];
+    console.log(firstCity.lat);
+    console.log(firstCity.lon);
+    var queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=49c5abbee178f817547829ce7c14a9e3`;
+    return fetch(queryURL);
+  })
 
-
-
-var queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=49c5abbee178f817547829ce7c14a9e3`;
-
-fetch(queryURL)
   .then((response) => response.json())
   .then((data) => {
     // do something with the data
     console.log(data);
   });
-
