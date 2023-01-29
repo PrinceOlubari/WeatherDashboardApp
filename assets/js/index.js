c6eb7caf72e5a0e6d748ad17feb1f3c54be7e686
Prince Olubari <olubari.prince.g@gmail.com>
2023-01-29 22:24:40 +0000

    added another fetch for the city names

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,14 @@
+
+var locationQuerryURL="http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=49c5abbee178f817547829ce7c14a9e3"
+fetch(locationQuerryURL)
+  .then((response) => response.json())
+  .then((data) => {
+    // do something with the data
+    console.log(data);
+  });
+
+
+
 var queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=49c5abbee178f817547829ce7c14a9e3`;
 
 fetch(queryURL)
